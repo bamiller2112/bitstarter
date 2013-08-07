e3acbb5c7ced7a1b6364f7c6063a439f0c5423b3
Brett Miller <brett.a.miller@gmail.com>
2013-08-07 20:19:14 +0000

    commented out port=8080 line and enabled original port assignment for heroku

--- web.js
+++ web.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var app = express();
-
 var fs = require('fs');
+
 var buffer = fs.readFileSync('index.html','utf8');
-
-
-
 
 
 app.get('/', function(request, response) {
   response.send(buffer);
 });
 
-//var port = process.env.PORT ||8080;
-var port = 8080;
+var port = process.env.PORT ||8080; // disabled on dev server
+//var port = 8080; // enabled on dev server for browser feedback
 
 app.listen(port, function() {
   console.log("Listening on " + port);
