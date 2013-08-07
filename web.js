1b91ca717765b29fa4f8da9ac13bbcbbd7a6862d
Brett Miller <brett.a.miller@gmail.com>
2013-08-07 19:07:05 +0000

    changed message content on index.html and corrected some typos;  updated web.js to read index.html and send contents to response header, added require('fs') to support file ops.

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
-app.use(express.logger());
+
+var fs = require('fs');
+var buffer = fs.readFileSync('index.html','utf8');
+
+
+
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
-var port = process.env.PORT || 5000;
+//var port = process.env.PORT ||8080;
+var port = 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
